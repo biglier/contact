951deb6614539cc15767a92e263730b1b1f555f5
biglier <lelyuk.stanislav@gmail.com>
2018-12-21 10:29:00 +0200

    add provider, change form

---
+++ Model/Contact/DataProvider.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: slava
+ * Date: 20.12.18
+ * Time: 16:26
+ */
+namespace Slavik\Contact\Model\Contact;
+
+use Slavik\Contact\Model\ResourceModel\Contact\CollectionFactory;
+
+class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
+{
+    /**
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
+     * @param \Slavik\Contact\Model\ResourceModel\Contact\CollectionFactory $contactCollectionFactory
+     * @param array $meta
+     * @param array $data
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $contactCollectionFactory,
+        array $meta = [],
+        array $data = []
+    ) {
+        $this->collection = $contactCollectionFactory->create();
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+    }
+
+    /**
+     * @return array
+     */
+    public function getData()
+    {
+        if (isset($this->loadedData)) {
+            return $this->loadedData;
+        }
+
+        $items = $this->collection->getItems();
+        $this->loadedData = array();
+        /** @var Customer $customer */
+        foreach ($items as $contact) {
+            // notre fieldset s'apelle "contact" d'ou ce tableau pour que magento puisse retrouver ses datas :
+            $this->loadedData[$contact->getId()]['contact'] = $contact->getData();
+        }
+
+
+        return $this->loadedData;
+
+    }
+}
